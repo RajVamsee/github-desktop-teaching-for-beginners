e3c21a02b01cee5936dbd42ee1102386752a0f4d
Raj Vamsee Jaggaraju <vamsee2312@gmail.com>
2026-07-28 00:39:45 -0500

    Feature Branch Hello

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ def main():
     print("Hello learners!")
     print("This is a simple beginner tutorial for anybody trying to get their hands on with Github Desktop and Python basics. Happy to help")
     print("Working on the feature branch!")
+    print("Hello from FEATURE branch!")
 
 if __name__ == "__main__":
     main()
